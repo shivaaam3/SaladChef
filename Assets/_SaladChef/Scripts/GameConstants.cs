7d6b550222ad2fa86f90217738a6c676f216a1c3
shivaaam3 <shivaaam3@gmail.com>
2018-04-21 13:55:11 +0530

    Player time increased to 3 minutes

--- Assets/_SaladChef/Scripts/GameConstants.cs
+++ Assets/_SaladChef/Scripts/GameConstants.cs
@@ -6,7 +6,7 @@ public class GameConstants{
 
 	public static float timeToChopVegetable = 3.0f;
 	public static float customerWaitingTimeForOneVeg = 30.0f;
-	public static float playerTime = 30.0f;
+	public static float playerTime = 180.0f;
 	public static int scoreIncrement = 30;
 	public static int scoreDecrementDustbin = -3;
 	public static int scoreDecrementCustomer = -5;
